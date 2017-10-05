1d591a91b6e078fc85535c1e82e128cb1832b41b
Julie Burke <jfburke@umich.edu>
2017-10-05 17:11:34 -0400

    finished 6b

--- hw6B.py
+++ hw6B.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 import re
 
 address = ' http://py4e-data.dr-chuck.net/known_by_Colton.html'
-#html = urllib.request.urlopen(url).read()
+
 totsum = 0
-#soup = BeautifulSoup(html, 'html.parser')
+
 n = 0
 x = 0
 while n < 7:
@@ -17,7 +17,7 @@ while n < 7:
         x = x + 1
         if x == 18:
             address = link.get('href', None)
-            #print (address)
+
             x = 0
             break
     n = n + 1
